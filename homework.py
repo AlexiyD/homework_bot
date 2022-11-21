a7f7522d5a27fac0f6ebe1be4320a8841f401105
Зубков Алексей <psi.alexeyzubkov@yandex.ru>
2022-11-22 00:29:38 +0300

    21.11.22_vol_2

--- homework.py
+++ homework.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
+    format='%(funcName)s, %(levelname)s, %(name)s, %(message)s',
     filename='main.log',
 )
 handler = [logging.FileHandler('log.txt'),
